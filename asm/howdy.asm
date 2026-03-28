29bcca400f978d731fdf108396a9996f270d2db1
Dustin Kirkland <dustin.kirkland@chainguard.dev>
2026-03-28 08:12:23 -0500

    license-headers: add blank comment line between license block and description

--- asm/howdy.asm
+++ asm/howdy.asm
@@ -11,6 +11,7 @@
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
+;
 ; Print a howdy greeting to stdout via Linux write syscall, then exit.
 section .data
     msg db "    ====> Assembly: Howdy!", 10
--- c/howdy.c
+++ c/howdy.c
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /* Print a howdy greeting to stdout. */
 #include <stdio.h>
 
